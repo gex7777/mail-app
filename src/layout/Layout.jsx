7b37e5a4ee8de172dd3d5b3e43736714d3ec46d1
George <gex7777@gmail.com>
2023-03-11 09:09:46 +0530

    change nested import for better vite support

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import PrimarySearchAppBar from "../components/CustomAppBar/CustomAppBar";
+import PrimarySearchAppBar from "../components/CustomAppBar";
 import { CustomDrawer } from "../components/CustomDrawer/CustomDrawer";
 
 const Layout = () => {
